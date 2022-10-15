ca43a88bb94cede978580c176bf5b7000c5e3291
VinceJenkins <114426417+VinceJenkins@users.noreply.github.com>
2022-10-15 16:13:57 +0530

    Create newfile.py

---
+++ newfile.py
@@ -0,0 +1,15 @@
+New Python program file after force push
+
+class Main{
+  public static void main(String[] arg) {
+    
+    // declare variables
+    int a = 12, b =5;
+    
+    // addition operator
+    System.out.println("a + b = " + (a + b));
+    
+    // Subtration operator
+    System.out.println("a - b = " + (a - b));
+  }
+}
